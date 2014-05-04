b48b4d48875b0d1d79b9128f03da9924a5cefa3f
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-05-05 01:28:59 +0200

    Ability to download file via waveform rest interface.

--- src/jane/filearchive/serializer.py
+++ src/jane/filearchive/serializer.py
@@ -8,11 +8,14 @@ from jane.filearchive import models
 class WaveformSerializer(serializers.HyperlinkedModelSerializer):
     plot = serializers.HyperlinkedIdentityField(
         view_name='waveform-plot', format='png')
+    containing_file = serializers.HyperlinkedIdentityField(
+        view_name='waveform-file', format='binary')
 
     class Meta:
         model = models.Waveform
-        fields = ['url', 'plot', 'network', 'station', 'location', 'channel',
-                  'starttime', 'endtime', 'sampling_rate', 'npts']
+        fields = ['url', 'plot', 'containing_file', 'network', 'station',
+                  'location', 'channel', 'starttime', 'endtime',
+                  'sampling_rate', 'npts']
 
 
 class PaginatedWaveformSerializer(pagination.PaginationSerializer):
--- src/jane/filearchive/views.py
+++ src/jane/filearchive/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 
 from rest_framework import viewsets, renderers
 
@@ -15,6 +16,13 @@ class PNGRenderer(renderers.BaseRenderer):
     def render(self, data, media_type=None, renderer_context=None):
         return data
 
+class BinaryRenderer(renderers.BaseRenderer):
+    media_type = "application/octet-stream"
+    format = "binary"
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return data
+
 
 class WaveformView(viewsets.ReadOnlyModelViewSet):
     queryset = models.Waveform.objects.all()
@@ -24,3 +32,13 @@ class WaveformView(viewsets.ReadOnlyModelViewSet):
     def plot(self, request, *args, **kwargs):
         obj = self.get_object()
         return Response(obj.preview_image)
+
+    @link(renderer_classes=[BinaryRenderer])
+    def file(self, request, *args, **kwargs):
+        file_obj = self.get_object().file
+        filename = os.path.join(file_obj.path.name, file_obj.name)
+        headers = {
+            "Content-Disposition": 'attachment; filename="%s"' % filename
+        }
+        with open(filename, "rb") as fh:
+            return Response(fh.read(), headers=headers)
