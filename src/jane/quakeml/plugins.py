b78f2273f1df4ad0c9b006bb28d0e3169271f6e9
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-10-26 15:07:21 +0100

    Indexing a couple more values for the events.

--- src/jane/quakeml/plugins.py
+++ src/jane/quakeml/plugins.py
@@ -27,7 +27,12 @@ class QuakeMLIndexerPlugin(IndexerPluginPoint):
             "depth_in_m": {"type": "float"},
             "origin_time": {"type": "datetime"},
             "magnitude": {"type": "float"},
-            "magnitude_type": {"type": "str"}
+            "magnitude_type": {"type": "str"},
+            "agency": {"type": "str"},
+            "author": {"type": "str"},
+            "public": {"type": "bool"},
+            "evaluation_mode": {"type": "str"},
+            "event_type": {"type": "str"}
         }
 
     def index(self, document):
@@ -45,6 +50,26 @@ class QuakeMLIndexerPlugin(IndexerPluginPoint):
             else:
                 mag = None
 
+            # Parse attributes in the baynet namespace.
+            if hasattr(event, "extra"):
+                if "public" in event.extra:
+                    public = event.extra["public"]["value"]
+                    if public.lower() in ["false", "f"]:
+                        public = False
+                    elif public.lower() in ["true", "t"]:
+                        public = True
+                    else:
+                        raise NotImplementedError
+                else:
+                    public = None
+                if "evaluationMode" in event.extra:
+                    evaluationMode = event.extra["evaluationMode"]["value"]
+                else:
+                    evaluationMode = None
+            else:
+                public = None
+                evaluationMode = None
+
             indices.append({
                 "latitude": org.latitude if org else None,
                 "longitude": org.longitude if org else None,
@@ -52,6 +77,11 @@ class QuakeMLIndexerPlugin(IndexerPluginPoint):
                 "origin_time": str(org.time) if org else None,
                 "magnitude": mag.mag if mag else None,
                 "magnitude_type": mag.magnitude_type if mag else None,
+                "agency": event.creation_info.agency_id,
+                "author": event.creation_info.author,
+                "public": public,
+                "evaluation_mode": evaluationMode,
+                "event_type": event.event_type,
                 "geometry":
                     [Point(org.longitude, org.latitude)] if org else None,
             })
