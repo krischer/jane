15e5b6fa09be34605bcc6015279bd8a07a4d0419
Robert Barsch <barsch@egu.eu>
2014-11-20 17:40:52 +0100

    adding autologgout middleware

--- src/jane/jane/middleware.py
+++ src/jane/jane/middleware.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-Add user created_by and modified_by foreign key to any model automatically.
-Almost entirely taken from https://github.com/Atomidata/django-audit-log/
-"""
+import time
+
+from django.conf import settings
+from django.contrib import auth
 from django.db.models import signals
 from django.utils.functional import curry
 
 
 class WhoDidItMiddleware(object):
+    """
+    Add user created_by and modified_by foreign key to any model automatically.
+    Almost entirely taken from https://github.com/Atomidata/django-audit-log/
+    """
     def process_request(self, request):
         if not request.method in ('GET', 'HEAD', 'OPTIONS', 'TRACE'):
             if hasattr(request, 'user') and request.user.is_authenticated():
@@ -27,3 +31,30 @@ class WhoDidItMiddleware(object):
             instance.created_by = user
         if hasattr(instance, 'modified_by_id'):
             instance.modified_by = user
+
+
+class AutoLogoutMiddleware(object):
+    def process_request(self, request):
+        # can't log out if not logged in
+        if not request.user.is_authenticated():
+            return
+
+        # check if auto logout is activated
+        try:
+            if not settings.AUTO_LOGOUT_MINUTES:
+                return
+        except:
+            return
+
+        try:
+            delta = time.time() - request.session['last_touch']
+        except (KeyError, TypeError):
+            pass
+        else:
+            seconds = settings.AUTO_LOGOUT_MINUTES * 60
+            if delta > seconds:
+                del request.session['last_touch']
+                auth.logout(request)
+                return
+
+        request.session['last_touch'] = time.time()
