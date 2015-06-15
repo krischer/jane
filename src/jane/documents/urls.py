06e7690f2dafdd2fcfdfcb63ecb89da1924a40fc
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-06-15 21:35:22 +0200

    cleaning up documents.urls

--- src/jane/documents/urls.py
+++ src/jane/documents/urls.py
@@ -10,14 +10,17 @@ urlpatterns = [
     # This view just returns the data of a document. Its thus a bit "hidden".
     url(r'^rest/__document_data__/(?P<pk>[0-9]+)$', views.document_data,
         name="document_data"),
+    # Root url for the documents.
     url(r'^rest/documents/?$', views.documents_rest_root),
+    # Root url for the indices.
     url(r'^rest/document_indices/?$', views.documents_indices_rest_root),
-    url(r'^rest/(?P<document_type>\w+)/$', views.record_list),
-    url(r'^rest/(?P<document_type>\w+)/(?P<pk>[0-9]+)/$',
-        views.record_detail),
-    url(r'^rest/(?P<document_type>\w+)/(?P<index_id>[0-9]+)/'
-        '(?P<attachment_id>[0-9]+)/$',
-        views.attachment_detail)
+
+    # url(r'^rest/(?P<document_type>\w+)/$', views.record_list),
+    # url(r'^rest/(?P<document_type>\w+)/(?P<pk>[0-9]+)/$',
+    #     views.record_detail),
+    # url(r'^rest/(?P<document_type>\w+)/(?P<index_id>[0-9]+)/'
+    #     '(?P<attachment_id>[0-9]+)/$',
+    #     views.attachment_detail)
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 
@@ -25,13 +28,11 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 for name, viewset in views.document_viewsets.items():
     router = OptionalTrailingSlashSimpleRouter(trailing_slash=False)
     router.register(name, viewset, base_name="rest_documents_%s" % name)
-
     urlpatterns.append(url(r'^rest/documents/', include(router.urls)))
 
 for name, viewset in views.document_index_viewsets.items():
     router = OptionalTrailingSlashSimpleRouter(trailing_slash=False)
     router.register(name, viewset, base_name="rest_document_indices_%s" % name)
-
     urlpatterns.append(url(r'^rest/document_indices/', include(router.urls)))
 
 
