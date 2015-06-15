35f854bc0ed0ad4e84658ac97dfdb18823268d32
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-06-16 00:22:58 +0200

    Linking new REST API

--- src/jane/documents/serializer.py
+++ src/jane/documents/serializer.py
@@ -7,18 +7,52 @@ from rest_framework_gis.serializers import GeoModelSerializer
 from jane.documents import models
 
 
+
+class DocumentTypeHyperlinkedIdentifyField(
+        serializers.HyperlinkedIdentityField):
+    def get_url(self, obj, view_name, request, format):
+        """
+        Given an object, return the URL that hyperlinks to the object.
+
+        May raise a `NoReverseMatch` if the `view_name` and `lookup_field`
+        attributes are not configured to correctly match the URL conf.
+        """
+        # Unsaved objects will not yet have a valid URL.
+        if obj.pk is None:
+            return None
+
+        lookup_value = getattr(obj, self.lookup_field)
+        kwargs = {self.lookup_url_kwarg: lookup_value}
+
+        # Deal with documents as well as indices.
+        if hasattr(obj, "document_type"):
+            kwargs["document_type"] = obj.document_type.name
+        else:
+            kwargs["document_type"] = obj.document.document_type.name
+
+        url = self.reverse(view_name, kwargs=kwargs, request=request,
+                           format=format)
+        return url
+
+
 class DocumentSerializer(serializers.ModelSerializer):
     data_url = serializers.HyperlinkedIdentityField(
         view_name="document_data", lookup_field="pk")
+
+    url = DocumentTypeHyperlinkedIdentifyField(
+        view_name='rest_documents-detail',
+        lookup_field="pk",
+        read_only=True)
 
     class Meta:
         model = models.Document
         fields = [
             'id',
             'name',
+            'url',
+            'data_url',
             'document_type',
             'content_type',
-            'data_url',
             'filesize',
             'sha1',
             'created_at',
@@ -29,8 +63,33 @@ class DocumentSerializer(serializers.ModelSerializer):
 
 
 class DocumentIndexSerializer(serializers.ModelSerializer):
+    url = DocumentTypeHyperlinkedIdentifyField(
+        view_name='rest_document_indices-detail',
+        lookup_field="pk",
+        read_only=True)
+
+    containing_document_url = DocumentTypeHyperlinkedIdentifyField(
+        view_name='rest_documents-detail',
+        lookup_field="document_id",
+        lookup_url_kwarg="pk",
+        read_only=True)
+
+    containing_document_data_url = serializers.HyperlinkedIdentityField(
+        view_name="document_data", lookup_field="document_id",
+        lookup_url_kwarg="pk")
+
+
     class Meta:
         model = models.DocumentIndex
+        fields = [
+            'id',
+            'url',
+            'containing_document_url',
+            'containing_document_data_url',
+            'json',
+            'geometry',
+            'created_at'
+        ]
 
 
 class AttachmentSerializer(serializers.ModelSerializer):
