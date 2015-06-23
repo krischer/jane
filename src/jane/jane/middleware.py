3cb4ffaaf748db0d5272a8375148ef6e774b8f67
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-06-23 22:47:19 +0200

    Removing whodidit middleware.
    Not needed in our case and it complicates things as we want command line
    as well as web interfaces.

--- src/jane/jane/middleware.py
+++ src/jane/jane/middleware.py
@@ -7,33 +7,6 @@ from django.db.models import signals
 from django.utils.functional import curry
 
 
-class WhoDidItMiddleware(object):
-    """
-    Add user created_by and modified_by foreign key to any model automatically.
-    Almost entirely taken from https://github.com/Atomidata/django-audit-log/
-    """
-    def process_request(self, request):
-        if request.method not in ('GET', 'HEAD', 'OPTIONS', 'TRACE'):
-            if hasattr(request, 'user') and request.user.is_authenticated():
-                user = request.user
-            else:
-                user = None
-            mark_whodid = curry(self.mark_whodid, user)
-            signals.pre_save.connect(mark_whodid,
-                                     dispatch_uid=(self.__class__, request,),
-                                     weak=False)
-
-    def process_response(self, request, response):
-        signals.pre_save.disconnect(dispatch_uid=(self.__class__, request,))
-        return response
-
-    def mark_whodid(self, user, sender, instance, **kwargs):  # @UnusedVariable
-        if not getattr(instance, 'created_by_id', None):
-            instance.created_by = user
-        if hasattr(instance, 'modified_by_id'):
-            instance.modified_by = user
-
-
 class AutoLogoutMiddleware(object):
     def process_request(self, request):
         # can't log out if not logged in
