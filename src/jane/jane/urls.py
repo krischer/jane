a23ef2a412cff01c9fc4921627854986935d4947
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-06-25 08:00:00 +0200

    Optional slash in url for root rest interface.

--- src/jane/jane/urls.py
+++ src/jane/jane/urls.py
@@ -9,7 +9,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = patterns(
     'jane.jane.views',
     url(r'^$', view='index'),
-    url(r'^rest/$', view='rest_root', name='rest_root'),
+    url(r'^rest/?$', view='rest_root', name='rest_root'),
     url(r'^rest/api-auth/', include('rest_framework.urls',
                                     namespace='rest_framework'))
 )
