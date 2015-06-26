55b23e8629ecc6d9d9ca6812f88a4b75ce37b8fc
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-06-26 13:18:44 +0200

    allowing colon as a valid filename

--- src/jane/documents/__init__.py
+++ src/jane/documents/__init__.py
@@ -4,4 +4,4 @@ default_app_config = "jane.documents.apps.JaneDocumentsConfig"
 
 # Regular expression used to check for a valid filename. Defined here as its
 # used in a number of places and thus its consistent.
-DOCUMENT_FILENAME_REGEX = r'[A-Za-z0-9-_.,]+'
+DOCUMENT_FILENAME_REGEX = r'[A-Za-z0-9-_.,:]+'
