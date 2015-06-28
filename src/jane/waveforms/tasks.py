1136a7e10970ee93bacb2fae5986d4bd06eec7c1
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-06-28 13:55:19 +0200

    filemonitoring no longer a task.
    The actual indexing still is but this really does not have to be.

--- src/jane/waveforms/tasks.py
+++ src/jane/waveforms/tasks.py
@@ -5,12 +5,11 @@ import json
 import gc
 import os
 
-from celery import shared_task
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import transaction
 from psycopg2.extras import DateTimeTZRange
-
 import matplotlib
+
+from celery import shared_task
+
 matplotlib.use("agg")  # NOQA
 import matplotlib.pylab as plt
 
@@ -150,105 +149,6 @@ def _format_return_value(event, message):
     return "Filemon event type: {event_type}, Result: {message}, Input: {" \
            "event}".format(event_type=event["event_type"], message=message,
                            event=str(event))
-
-
-@shared_task
-def filemon_event(event):
-    """
-    Handle file monitor events
-    """
-    # Possible event types: created, deleted, modified, moved
-    event_type = event['event_type']
-    is_directory = event["is_directory"]
-
-    # Assertions to gain trust in the async flow.
-    assert event_type in ("created", "modified", "deleted", "moved")
-    assert is_directory in (True, False)
-
-    # Deal with files first.
-    if is_directory is False:
-        src_path = event['src_path']
-        src_file = os.path.basename(src_path)
-        src_folder = os.path.dirname(src_path)
-
-        if event_type in ("created", "modified"):
-            # New or modified file.
-            process_file.delay(filename=src_path)
-            return _format_return_value(event, "File sent to processing.")
-        # Delete file object if file has been deleted.
-        elif event_type == "deleted":
-            try:
-                models.File.objects.get(path__name=src_folder,
-                                        name=src_file).delete()
-                return _format_return_value(event, "File deleted.")
-            except ObjectDoesNotExist:
-                return _format_return_value(event, "File already deleted.")
-        elif event_type == "moved":
-            dest_file = os.path.basename(event['dest_path'])
-            dest_folder = os.path.dirname(event['dest_path'])
-
-            # Nothing happened.
-            if src_path == event["dest_path"]:
-                return _format_return_value(event, "File not moved.")
-
-            with transaction.atomic():
-                dest_path_obj = models.Path.objects.get_or_create(
-                    name=dest_folder)[0]
-                dest_path_obj.save()
-
-                src_file_obj = models.File.objects.get(
-                    path__name=src_folder, name=src_file)
-
-                src_file_obj.name = dest_file
-                src_file_obj.path = dest_path_obj
-                src_file_obj.save()
-
-            # Check if the src_path has files left in it. If not, try to
-            # delete it.
-            try:
-                src_path_obj = models.Path.objects.get(name=src_folder)
-            except ObjectDoesNotExist:
-                return _format_return_value(event, "File moved, path already "
-                                                   "deleted.")
-
-            if src_path_obj.files.count() == 0:
-                try:
-                    src_path_obj.delete()
-                except AssertionError:
-                    return _format_return_value(event, "File moved, deleting "
-                                                       "path failed.")
-                return _format_return_value(event, "File moved, old path "
-                                                   "deleted.")
-            return _format_return_value(event, "File moved, path untouched.")
-        # Should not happen.
-        else:
-            raise JaneWaveformTaskException(
-                "Invalid watchdog event type: '%s'" % event_type)
-    # Deal with directories.
-    else:
-        src_folder = os.path.abspath(event['src_path'])
-        if event_type == "deleted":
-            try:
-                models.Path.objects.get(name=src_folder).delete()
-                return _format_return_value(event, "Deleted path.")
-            except ObjectDoesNotExist:
-                return _format_return_value(event, "Failed deleting path.")
-        elif event_type == "moved":
-            # Only deal with it if the directory actually exists in the
-            # database.
-            try:
-                path_obj = models.Path.objects.get(name=src_folder)
-            except ObjectDoesNotExist:
-                return _format_return_value(event, "File could not be moved.")
-            # If it does, just update the path.
-            path_obj.name = os.path.abspath(event['dest_path'])
-            path_obj.save()
-            return _format_return_value(event, "Moved path.")
-        # Should not happen. Modified and created directories are not passed
-        # to the task queue.
-        else:
-            raise JaneWaveformTaskException(
-                "Invalid watchdog event type: '%s'" % event_type)
 
 
 @shared_task
