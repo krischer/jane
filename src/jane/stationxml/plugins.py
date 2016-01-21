422980fac88a9d98e903aaba62218d708c4c2952
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-01-21 12:05:16 +0100

    Indexing a couple more fields.
    That way the overviews can be generated much quicker via the FDSN
    services.

--- src/jane/stationxml/plugins.py
+++ src/jane/stationxml/plugins.py
@@ -45,10 +45,15 @@ class StationIndexerPlugin(IndexerPluginPoint):
         "longitude": "float",
         "elevation_in_m": "float",
         "depth_in_m": "float",
+        "azimuth": "float",
+        "dip": "float",
         "start_date": "UTCDateTime",
         "end_date": "UTCDateTime",
         "sample_rate": "float",
-        "sensor_type": "str"
+        "sensor_type": "str",
+        "total_sensitivity": "float",
+        "sensitivity_frequency": "float",
+        "units_after_sensitivity": "str"
         }
 
     def index(self, document):
@@ -57,7 +62,23 @@ class StationIndexerPlugin(IndexerPluginPoint):
         for network in inv:
             for station in network:
                 for channel in station:
+                    if channel.response:
+                        if channel.response.instrument_sensitivity:
+                            _i = channel.response.instrument_sensitivity
+                            total_sensitivity = _i.value
+                            sensitivity_frequency = _i.frequency
+                            units_after_sensitivity = _i.input_units
+                        else:
+                            total_sensitivity = None
+                            sensitivity_frequency = None
+                            units_after_sensitivity = None
+                    else:
+                        total_sensitivity = None
+                        sensitivity_frequency = None
+                        units_after_sensitivity = None
+
                     index = {
+                        # Information.
                         "network": network.code,
                         "network_name": network.description,
                         "station": station.code,
@@ -65,16 +86,30 @@ class StationIndexerPlugin(IndexerPluginPoint):
                         station.description else station.site.name,
                         "location": channel.location_code,
                         "channel": channel.code,
+
+                        # Coordinates and orientation.
                         "latitude": channel.latitude,
                         "longitude": channel.longitude,
                         "elevation_in_m": channel.elevation,
                         "depth_in_m": channel.depth,
+                        "dip": channel.dip,
+                        "azimuth": channel.azimuth,
+
+                        # Dates.
                         "start_date": str(channel.start_date),
+                        "end_date": str(channel.end_date)
+                        if channel.end_date else None,
+
+                        # Characteristics.
                         "sample_rate": float(channel.sample_rate),
                         "sensor_type": channel.sensor.type
                         if channel.sensor else None,
-                        "end_date": str(channel.end_date)
-                        if channel.end_date else None,
+                        # Some things have to be extracted from the response.
+                        "total_sensitivity": total_sensitivity,
+                        "sensitivity_frequency": sensitivity_frequency,
+                        "units_after_sensitivity": units_after_sensitivity,
+
+                        # Geometry for PostGIS.
                         "geometry": [Point(channel.longitude,
                                            channel.latitude)],
                     }
