a6cf70f47c7538398ecae9d0887c7bf9ef825529
Lion Krischer <lion.krischer@gmail.com>
2017-05-11 15:20:07 +0200

    Somehow ugly fix for the response plot in the web GIS.

--- src/jane/static/web_gis/src/controllers/station_info_controller.js
+++ src/jane/static/web_gis/src/controllers/station_info_controller.js
@@ -1,8 +1,8 @@
 var baynetApp = angular.module("bayNetApp");
 
 
-baynetApp.controller('stationInfoController', function($scope, $log, stations) {
-    for (var i=0; i < stations.stations.features.length; i++) {
+baynetApp.controller('stationInfoController', function ($scope, $log, stations) {
+    for (var i = 0; i < stations.stations.features.length; i++) {
         var j = stations.stations.features[i];
         if (j.properties.network !== $scope.network ||
             j.properties.station != $scope.station) {
@@ -12,7 +12,19 @@ baynetApp.controller('stationInfoController', function($scope, $log, stations) {
         $scope.network_name = j.properties.network_name;
         $scope.station_name = j.properties.station_name;
         $scope.channels = j.properties.channels;
-        $log.info($scope.channels);
+        for (var i = 0; i < $scope.channels.length; i++) {
+            var i = $scope.channels[i];
+            // A bit ugly but I'm currently not sure how to do it in a
+            // better way. Should work well enough I guess.
+            jQuery.ajax({
+                url: i["attachments_url"],
+                success: function (result) {
+                    i["attachments"] = result.results;
+                },
+                async: false
+            });
+            console.log(i);
+        }
         break;
     }
 });
