61bc79f6e0468998ece65a82f2699474acd77d4c
Tobias Megies <megies@geophysik.uni-muenchen.de>
2017-10-27 15:00:04 +0400

    add helper method to work around django jsonfield "__isnull" query bugs
    checking for null values in jsonfields is buggy, so work around it as
    described here using "__contains" query type: https://stackoverflow.com/q/38528516

--- src/jane/jane/utils.py
+++ src/jane/jane/utils.py
@@ -17,3 +17,33 @@ class OptionalTrailingSlashSimpleRouter(SimpleRouter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.trailing_slash = "/?"
+
+
+def _queryset_filter_jsonfield_isnull(queryset, path, isnull, field='json'):
+    """
+    Replaces the buggy isnull query on json fields, see
+    https://stackoverflow.com/q/38528516
+
+    :type queryset: :class:`django.db.models.query.QuerySet`
+    :param queryset: Django queryset object to do the query on.
+    :type field: str
+    :param field: Name of the field (column) in the databse table that holds
+        the json data for the query. By default should always be "json" for
+        Jane.
+    :type path: list
+    :param path: List of field names as strings to traverse in the json field.
+        For example use ``field='json', path=['end_date'], isnull=True``, for a
+        query of type ``queryset.filter(json__end_date__isnull=True)``.
+    :type isnull: bool
+    :param isnull: Whether to return items with the respective field being
+        `null` (``isnull=True``) or it *not* being `null` (``isnull=False``).
+    """
+    key = '__'.join([field] + list(path[:-1]) + ['contains'])
+    if not path:
+        raise ValueError()
+    kwargs = {key: {path[-1]: None}}
+    if isnull:
+        method = queryset.filter
+    else:
+        method = queryset.exclude
+    return method(**kwargs)
